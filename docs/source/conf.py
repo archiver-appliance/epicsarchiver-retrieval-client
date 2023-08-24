715e5e7dfefd35ecfb2f4d6caa07889522bdc3cb
Sky Brewer <sky.brewer@ess.eu>
2023-08-24 17:24:50 +0200

    fix docs with new project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ from importlib.metadata import version as get_version
 project = "epicsarchiver"
 copyright = "2023, European Spallation Source ERIC"  # noqa: A001
 authors = ["Benjamin Bertrand", "Sky Brewer"]
-release = get_version("epicsarchiver")
+release = get_version("py-epicsarchiver")
 version = ".".join(release.split(".")[0:2])
 
 
