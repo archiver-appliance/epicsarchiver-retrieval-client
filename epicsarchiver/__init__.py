b5001d4cb3352086ef431eec8123e3f3f11cbee9
Sky Brewer <sky.brewer@ess.eu>
2024-10-21 10:20:08 +0200

    Fix F822 lint

--- epicsarchiver/__init__.py
+++ epicsarchiver/__init__.py
@@ -1,10 +1,13 @@
 """Archiver appliance module."""
 
+from epicsarchiver import common, epicsarchiver, mgmt, retrieval, statistics
+
 __all__ = [
+    "common",
     "epicsarchiver",
     "mgmt",
     "retrieval",
-    "statistics",
+    "statistics"
 ]
 
 from epicsarchiver.epicsarchiver import (
