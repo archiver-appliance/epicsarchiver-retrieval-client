b3c375df0fc82b4a16e46b290dc33c377352885e
Sky Brewer <sky.brewer@ess.eu>
2024-12-06 10:13:06 +0100

    Use auto for enum initialisation

--- epicsarchiver/common/__init__.py
+++ epicsarchiver/common/__init__.py
@@ -1,23 +1,23 @@
 """Shared utilities across the other modules."""
 
-from enum import Enum
+from enum import Enum, auto
 
 
 class ArchDbrType(Enum):
     """List of Dbr Types that the archiver uses."""
 
-    DBR_SCALAR_STRING = 0
-    DBR_SCALAR_SHORT = 1
-    DBR_SCALAR_FLOAT = 2
-    DBR_SCALAR_ENUM = 3
-    DBR_SCALAR_BYTE = 4
-    DBR_SCALAR_INT = 5
-    DBR_SCALAR_DOUBLE = 6
-    DBR_WAVEFORM_STRING = 7
-    DBR_WAVEFORM_SHORT = 8
-    DBR_WAVEFORM_FLOAT = 9
-    DBR_WAVEFORM_ENUM = 10
-    DBR_WAVEFORM_BYTE = 11
-    DBR_WAVEFORM_INT = 12
-    DBR_WAVEFORM_DOUBLE = 13
-    DBR_V4_GENERIC_BYTES = 14
+    DBR_SCALAR_STRING = auto()
+    DBR_SCALAR_SHORT = auto()
+    DBR_SCALAR_FLOAT = auto()
+    DBR_SCALAR_ENUM = auto()
+    DBR_SCALAR_BYTE = auto()
+    DBR_SCALAR_INT = auto()
+    DBR_SCALAR_DOUBLE = auto()
+    DBR_WAVEFORM_STRING = auto()
+    DBR_WAVEFORM_SHORT = auto()
+    DBR_WAVEFORM_FLOAT = auto()
+    DBR_WAVEFORM_ENUM = auto()
+    DBR_WAVEFORM_BYTE = auto()
+    DBR_WAVEFORM_INT = auto()
+    DBR_WAVEFORM_DOUBLE = auto()
+    DBR_V4_GENERIC_BYTES = auto()
