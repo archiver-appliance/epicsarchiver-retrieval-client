2d8b2c741757b4603ce83939ba53ed77a67ce8bc
Sky Brewer <sky.brewer@ess.eu>
2025-04-01 14:52:00 +0200

    Remove the statistics module

--- epicsarchiver/statistics/models/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Models for archiver statistics report."""
--- epicsarchiver/statistics/reports/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Reports for the statistics module."""
-
-REPORT_CSV_HEADINGS = [
-    "IOC Name",
-    "IOC hostname",
-    "PV name",
-    "Statistic",
-    "Statistic Note",
-]
