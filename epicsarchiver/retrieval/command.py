4f4e10738200db58c586adf118f658a9f216d416
Jason Brudvik <jason.brudvik@ess.eu>
2026-02-09 08:57:07 +0100

    update CLI search function to accept single string
    remove option for more than one input string

--- epicsarchiver/retrieval/command.py
+++ epicsarchiver/retrieval/command.py
@@ -183,11 +183,11 @@ def get(  # noqa: PLR0917, PLR0913
     callback=handle_debug,
     help="Turn on debug logging",
 )
-@click.argument("pvstrings", type=str, required=True, nargs=-1)
+@click.argument("pv-glob-search", type=str, required=True, nargs=1)
 @click.pass_context
 def search(  # noqa: PLR0917, PLR0913
     ctx: click.core.Context,
-    pvstrings: tuple[str],
+    pv_glob_search: str,
     start: datetime | None,
     end: datetime | None,
     limit: int,
@@ -198,24 +198,25 @@ def search(  # noqa: PLR0917, PLR0913
     Optionally specify start and/or end times to only return PVs that recorded data in
     the specified time range.
 
-    ARGUMENT pvstrings Multiple strings to search for, use glob search characters
+    ARGUMENT pv-glob-search PV name search pattern, use glob search characters, case
+    insensitive
 
     Example usage:
 
     .. code-block:: console
 
         epicsarchiver --hostname archiver-01.example.com search         \
-        PBI-APTM02:Ctrl-ECAT-100:*Temp1[2-4]*   mbl*0[6-7]0*ambient*
+        PBI-APTM02:Ctrl-ECAT-100:*Temp1[2-4]*
 
         epicsarchiver --hostname archiver-01.example.com search         \
         PBI-APTM02:* -s "2026-01-06 02:50:00"
 
         epicsarchiver --hostname archiver-01.example.com search         \
-        MBL*:RFS*:*TempAmbient -s "2026-01-05"  -e "2026-01-06"
+        mbl*0[6-7]0*ambient* -s "2026-01-05"  -e "2026-01-06"
 
     """
     archiver: ArchiverAppliance = ctx.obj["archiver"]
-    LOG.debug("Search strings: %s", pvstrings)
+    LOG.debug("Search string: %s", pv_glob_search)
     LOG.debug("Limit: %s", limit)
     LOG.debug("Start: %s", start)
     LOG.debug("End:   %s", end)
@@ -223,7 +224,7 @@ def search(  # noqa: PLR0917, PLR0913
         pv_name_list = asyncio.run(
             _pv_name_search(
                 archiver=archiver,
-                pvstrings=pvstrings,
+                pv_glob_search=pv_glob_search,
                 start=start,
                 end=end,
                 limit=limit,
@@ -431,12 +432,12 @@ async def _single_fetch_events(
 
 async def _pv_name_search(
     archiver: ArchiverAppliance,
-    pvstrings: tuple[str],
+    pv_glob_search: str,
     start: datetime | None,
     end: datetime | None,
     limit: int,
 ) -> list[str]:
     async with AsyncArchiverRetrieval(archiver.hostname, archiver.port) as a_retrieval:
         return await a_retrieval.search(
-            pvstrings=pvstrings, start=start, end=end, limit=limit
+            pv_glob_search=pv_glob_search, start=start, end=end, limit=limit
         )
